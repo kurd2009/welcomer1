2440d180d08f0d4052854d2627590e875b6d1ede
kurd2009 <150278754+kurd2009@users.noreply.github.com>
2023-11-09 15:03:11 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const botconfig = require(`./botconfig.json`);
+const colors = require(`./colors.json`);
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.once('ready', () => {
+    console.log(`${client.user.tag} is ready`);
+});
+client.on('guildMemberAdd', member => {
+    let welcomeChannel = client.channels.cache.get("1033452301970583684")
+    let targetchannel = '1010839612702330953'
+    if(welcomeChannel){
+
+        let welcomeEmbed = new discord.MessageEmbed()
+        if(member.user.bot){
+
+        if(member.user.client){
+
+            welcomeEmbed.setColor(colors.yellow)
+            welcomeEmbed.setAuthor(`${member.guild.name}`)
+            welcomeEmbed.setDescription(`سلام <@${member.user.id}> به سرور ما خوش اومدی حتما یه سر بزن به ${member.guild.channels.cache.get(targetchannel).toString()} امیدوارم از سرورمون خوشت بیاد`)
+            welcomeEmbed.setThumbnail(member.user.displayAvatarURL())
+            welcomeEmbed.setFooter(`${client.user.username} (=`)
+            welcomeChannel.send(welcomeEmbed)
+        }}else{
+            welcomeEmbed.setColor(colors.aqua)
+            welcomeEmbed.setAuthor(`${member.guild.name}`)
+            welcomeEmbed.setDescription(`سلام <@${member.user.id}> به سرور ما خوش اومدی حتما یه سر بزن به ${member.guild.channels.cache.get(targetchannel).toString()} امیدوارم از سرورمون خوشت بیاد`)
+            welcomeEmbed.setThumbnail(member.user.displayAvatarURL())
+            welcomeEmbed.setFooter(`${client.user.username}  (=  `)
+            welcomeChannel.send(welcomeEmbed)
+        }
+    }else{
+        console.log("Welcome Channel Yaft Nashod")
+    }
+})
+client.on("ready", () => {
+    function YousamPower() {
+      let hungry = ["Kurd Shop" , "Listen To Feedback", "14 customer" ]
+      let Power = Math.floor(Math.random() * hungry.length);
+      client.user.setActivity(hungry[Power], {type: "STREAMING", url: "https://www.twitch.tv/Kurd.Shop"});
+    }; setInterval(YousamPower, 2000)
+  });
+
+
+client.login(botconfig.token);
+
